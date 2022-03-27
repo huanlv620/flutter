79ac1c07eda7e32fec5036f6f86bef17bd77e06b
huanlv620 <huanliendoanh@gmail.com>
2022-03-27 22:14:03 +0700

    huan

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_chat_firebase/screens/splash_screen.dart';
+import 'package:flutter_app_chat_firebase/theme.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+
+import 'app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  final client = StreamChatClient(streamKey);
+
+  runApp(
+    MyApp(
+      client: client,
+      appTheme: AppTheme(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required this.client,
+    required this.appTheme,
+  }) : super(key: key);
+
+  final StreamChatClient client;
+  final AppTheme appTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: appTheme.light,
+      darkTheme: appTheme.dark,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      title: 'Chatter',
+      builder: (context, child) {
+        return StreamChatCore(
+          client: client,
+          child: ChannelsBloc(
+            child: UsersBloc(
+              child: child!,
+            ),
+          ),
+        );
+      },
+      home: const SplashScreen(),
+    );
+  }
+}
